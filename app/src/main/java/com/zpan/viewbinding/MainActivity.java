a9bd4a0281e03585cdfbe3c451f910c526124bb1
张攀攀 <zhang.panpan@towngas.com.cn>
2020-04-20 16:28:33 +0800

    引入ViewBinding

--- app/src/main/java/com/zpan/viewbinding/MainActivity.java
+++ app/src/main/java/com/zpan/viewbinding/MainActivity.java
@@ -1,16 +1,21 @@
 package com.zpan.viewbinding;
 
+import android.view.LayoutInflater;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.zpan.viewbinding.databinding.ActivityMainBinding;
 
 /**
  * @author zpan
  */
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding mainBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = ActivityMainBinding.inflate(LayoutInflater.from(this));
+        setContentView(mainBinding.getRoot());
     }
 }
